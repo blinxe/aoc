cf4602390e33537364934b24926e8951da348d8f
Benoît Linxe <benoit.linxe@idemia.com>
2019-04-03 11:27:12 +0200

    Add days 1-4

---
+++ aoc-2.py
@@ -0,0 +1,44 @@
+ls = ['icxjubroqtunzeyzpomfksahgw', 'ibxjvbroqtunleyzjdmfksahow', 'icxjvbroqtinleyzpdmflsahnw', 'icxjvbnoqtunleyvpgmfksahgw', 'wcxjvbroqrunleyzpdmfksahge', 'icxjtbroqtjzleyzpdmfksahgw', 'icxjvbrohtunleyzpdmfkbahsw', 'xcxjvbroqcunleyzpdmfksaxgw', 'ycxjvbroqtunleyzpowfksahgw', 'icxfvbroqtunleyzpdmfksncgw', 'ixxjvbuoqtunleyzpdvfksahgw', 'icfjvbroqtunleyzpdmfksadgt', 'icxjvbroqdunleyzpdafksqhgw', 'icxjvbrobtunlelzpdmfkuahgs', 'ujxjvbroqtunleyzpdmqksahgw', 'icqjvsroqtunleyzpdmfksahuw', 'icxjvbroptpnleyzpdmfksangw', 'ipxjvbroqtunleyzpdmfesahgi', 'icajvbroqtunltyzpdqfksahgw', 'ickjvbroqtuzleyzpdmfgsahgw', 'icxjvbroqtunledzpdmwksahgz', 'icxjvlroqtsnleyzpdmfksvhgw', 'icxjvbroqtunleyzpdsfysahvw', 'icxjvbroqtunwnyzydmfksahgw', 'ionjvbroqtunleyzpdmfksahgj', 'icxjvwriqtunleyzpdmfksahgi', 'ocxjvbroztunleyzpdmfksapgw', 'icxjvbroqtmnlewzpumfksahgw', 'ucxjvbroqtunleyzpdmzktahgw', 'icxgvbroqtunleyztdmfktahgw', 'icxhvbroqttnleybpdmfksahgw', 'icxjvbroqtugleyzpdxfkeahgw', 'acxjvbroqvunlexzpdmfksahgw', 'icxjvbroqglnleyzpbmfksahgw', 'icxjvbriqtnvleyzpdmfksahgw', 'icxjvbreqtunlakzpdmfksahgw', 'gcxjvbuoqtunleyzpdmfksawgw', 'icxjvbroqtunleyzpddfkyzhgw', 'icxjvbjoqtunleyzpdmfqsahhw', 'icxjvjroqtunleyzpnmfksajgw', 'ycxjvbroqtunmeyzcdmfksahgw', 'irxkvbryqtunleyzpdmfksahgw', 'isxjvbrlqtunleyzpdmsksahgw', 'icxjvbcoqtunleyzpdfkksahgw', 'ixnjvbroqtunleyzpdmfkqahgw', 'wcxjvbroqhunleyzqdmfksahgw', 'icljvurmqtunleyzpdmfksahgw', 'ibxjvbroqtayleyzpdmfksahgw', 'arxjvbroqiunleyzpdmfksahgw', 'iuxjvbroqtunluyzpdmoksahgw', 'icxjvbrmqtunleyzpdmfosahew', 'isxjvbroqtunleyrpdmfksrhgw', 'icxjvxrpqtunleyzpdmfkdahgw', 'ichjvbrogtunllyzpdmfksahgw', 'icxjvbeoqtunlryzpdmfksakgw', 'icxjvbroqtcnemyzpdmfksahgw', 'icxjvbroqtybledzpdmfksahgw', 'icxjvbrqqtunleyzpdmfksgngw', 'icgjvbroqtunleyzmdmfksabgw', 'icxjtbroqtunleyzkdmfksahww', 'icxjvbfoqtunleyzpamfqsahgw', 'icxjvbroknuyleyzpdmfksahgw', 'icxjvbroqtujleyzpdmaksaigw', 'icxjvbroqtnnlmyzpdmflsahgw', 'icxjvbroqtunlefzpdmfsfahgw', 'icxjvdroqtusleyzpdzfksahgw', 'icxjvbioqtunlsyzpdmfkshhgw', 'icxbvbrodtunleyzpdmoksahgw', 'icxjvbroqtuvleyzpdmfkbahmw', 'iyxjvbroqtunljvzpdmfksahgw', 'icxjvbroqtudleynddmfksahgw', 'icxjvwroqtnnleyzpdmfksahgz', 'ichjvbroqtunleyzjdmeksahgw', 'icxjvbrostunluyrpdmfksahgw', 'icfjvbroqtunleyxpdgfksahgw', 'nhxjvbroqtunlerzpdmfksahgw', 'icxjvbrothunlexzpdmfksahgw', 'icxjvbrzltqnleyzpdmfksahgw', 'icxjvbrhqtunleyzpdmfksajgy', 'vcxjvjroqiunleyzpdmfksahgw', 'icxjfbroltunleyzpdmqksahgw', 'icxbvbroqtunleyzpdofasahgw', 'icxjvbkoqtunveyzpdmfksaqgw', 'icxsebroqtunleyzpdmuksahgw', 'icxjvbroquunlpyrpdmfksahgw', 'icxhvbroqtunjeyzpdmrksahgw', 'icdjvbroqtunlzyzpdmfksangw', 'jcxqvbroqtvnleyzpdmfksahgw', 'icxjvxroqtunleyrpdmfxsahgw', 'icxjvnroqtunleyzpdmfssyhgw', 'icxjvbraptunleyzpdofksahgw', 'icxjvbroatunleyjpdmfbsahgw', 'icxjvbroytlnlryzpdmfksahgw', 'iaxjvbroqkunleyzpdmfcsahgw', 'ucxjvbroqtuileyzzdmfksahgw', 'icxjqbroqtcnleyzpgmfksahgw', 'icxjvbloqtunleyzadmfksaqgw', 'icxjvbroqtunleyzkdmnksakgw', 'icxjvbroqtunleyjpdxfksahvw', 'iqxjvbroqtujleyzpdmfklahgw', 'icgjvbroqtunleyzpdmfksbhgb', 'icxjzbroqtunleyzpdmfkdahgg', 'icxjvbrobtunloywpdmfksahgw', 'icxavbroqtunleyfpdmfksahgd', 'icxjvbroqtunleyophmfksahkw', 'icxjndroqtunlyyzpdmfksahgw', 'icxjvbroqtjnleyppdmvksahgw', 'icxjvbroonfnleyzpdmfksahgw', 'icxjvbrqqtlnljyzpdmfksahgw', 'icxjvbrzqtunlelspdmfksahgw', 'icxjvbooqtunleyztdmfkfahgw', 'icajvbroltunlnyzpdmfksahgw', 'icxjvbroqtunleyzidmdkschgw', 'icxjvbroktupleyzpdmfksahyw', 'icxjcbroyeunleyzpdmfksahgw', 'icxjvbroqtunlezkpdmfksahsw', 'icxjvbroqtunlejzpcmfksrhgw', 'icxjvvroqtunlsyzkdmfksahgw', 'icxjnbroqtunbeyzpdmfpsahgw', 'itxjbbroqtunleyzpemfksahgw', 'icxjvbroqtunlrkzpdmfksshgw', 'rcxjvbroqtujlbyzpdmfksahgw', 'icxjvmroqtugleazpdmfksahgw', 'icxjvbfoqtunleylpdmfkeahgw', 'icnjvoroktunleyzpdmfksahgw', 'icxjvbroqtunlvyzpdmfkfahgr', 'icxjvbroqtgnseyzpdmfxsahgw', 'scxjvbroqtunleycpdmfksdhgw', 'icxhvbxoqtunleuzpdmfksahgw', 'icxjvbruqtunleyzpnmfksfhgw', 'icdjvbroqtunleyzpdmfksahup', 'ihxjvbroqtunleovpdmfksahgw', 'icxjvbroqtunleyzxdmfksahzv', 'ocxjvbioqtunleyzpdmfzsahgw', 'idxjvbroqtunlyyzpdofksahgw', 'izdjvbroqtunleyzpdtfksahgw', 'icxjvbrnqtunleyzpdmfksbhgb', 'icxjvbrjqtunleyhpdmrksahgw', 'icxjvbroqtunleyzpdbflsahgg', 'icxjvbmfqtunleyzpdmfkaahgw', 'idxjvbroqtunlsyzpdffksahgw', 'bcxjvbroqtunleyzpkmfkswhgw', 'ivxjvbroqtdnleyzpdmbksahgw', 'icxpvbboqtunleyzpdmfksahtw', 'ibxjvbroqtunlehzpdmfkmahgw', 'icxjvbboqtunleyzpdmfkaahgv', 'icxjlaroqtuileyzpdmfksahgw', 'icxjvbroftunleyzpdmfqsahew', 'ichjvbroqtunleyzpdmiwsahgw', 'icxrvbvoqtunleyzpdmiksahgw', 'icxjvbroqtunldydpdmfksahgl', 'icogvbroqtunleyzpdmfnsahgw', 'icxjvbroqtunleszodmfkswhgw', 'icxjvbrontunleyztemfksahgw', 'icxjvbrovtunleyzpdvkksahgw', 'icxjvbroqqucteyzpdmfksahgw', 'icmovbroptunleyzpdmfksahgw', 'icxjvbqoftunleyzvdmfksahgw', 'icxjvbdoqtunleyzpdmfkadhgw', 'icxjvbroqtunlgnzpdmfksaqgw', 'icxjvbroqtunieygpdyfksahgw', 'acdjvbroqtunleyzpdmfkwahgw', 'icxjvbroqtunleyzpdmfkfahlj', 'icxjvbgoqtunleyepdmvksahgw', 'icxjvbpobbunleyzpdmfksahgw', 'icxjvbroqtunleurpdmfktahgw', 'ipxjvbzoqtunleyzpdmffsahgw', 'icxjtbroqtunllyzpdmuksahgw', 'icxjvbroqtunbsyzadmfksahgw', 'ihxjvoroqtuqleyzpdmfksahgw', 'idxjmbroqqunleyzpdmfksahgw', 'wcxjvbdoqtunleyzpdmfksahgr', 'icxjvbroqtunleygptmfksahgj', 'ipxjvbrsqtunleyzpdmfksghgw', 'ycxjvbroqtunluyzkdmfksahgw', 'icxjvbroxtuulejzpdmfksahgw', 'icqjvbroqtunlwyypdmfksahgw', 'ioxjhbroqtunleyzphmfksahgw', 'icxjvbgoqnunleyzpdmfksahaw', 'mcxjvbroqtunleyzpdmfksihgh', 'icxjsbroqtunlqyzpdmfksawgw', 'icxjvbroqtuoleycpdmftsahgw', 'icxjvbroqtunleyzgdifksahlw', 'icxjvbmoqtunleyzjfmfksahgw', 'icxjvbroqtunlezopdmfksahge', 'icxjvbroqtbnlefzpdmfosahgw', 'tcxjvbromtunlevzpdmfksahgw', 'irxjgbroqtunleyzpdmfksthgw', 'icxjvbrojtunleyxpdmoksahgw', 'icxrvbroytpnleyzpdmfksahgw', 'icxjvbroqtunfeyupdmfksasgw', 'ihqjvbroqtunleyzpdmftsahgw', 'icxjobroqkunleozpdmfksahgw', 'icjjjbroqtualeyzpdmfksahgw', 'icxjvbroqtunaeytpdmfksahvw', 'icxjvbroqtunzeyzpdmfkshhxw', 'icxqvbroqtucleyzxdmfksahgw', 'icxjvbrogturleyzxdmfksahgw', 'icxjvoqoqtunleyzpdcfksahgw', 'iuxjvbroqtunleyzpdmfksopgw', 'icxjveroqtunleyzptmfksalgw', 'icxjvbroqtunleyzpdmfwcahhw', 'iwxjvbroqtlnleyzpdmfksyhgw', 'ectjvbroqtanleyzpdmfksahgw', 'icxjvnroqtyhleyzpdmfksahgw', 'icvjvhboqtunleyzpdmfksahgw', 'icxjtbroqtuzleyupdmfksahgw', 'icjjvproqtunleyzpsmfksahgw', 'icdjvbroqtutleyzpdmiksahgw', 'icxjvwroqtujleyzpdmfksahgc', 'icxjxbroqtunleyzpdwhksahgw', 'icxjvbqoqtunleyzpdmvfsahgw', 'icajvbroqtusleyzpdmfksaagw', 'icxjvbroqtunbtyzpdmfksmhgw', 'kcxjvbroqtxnleyzpdmfkskhgw', 'icxjvbqogfunleyzpdmfksahgw', 'icxjvbroqtubleyzpdmfdswhgw', 'icxjvprlqtunleyzpdmffsahgw', 'icxjxbroqtucleyzpdmfksakgw', 'dcxrvbroqtunleycpdmfksahgw', 'icxjvbrobtunleyzpomfksahgu', 'ocxrvbroqtunleyzpdmfssahgw', 'icxjvbroktunlejzpdmfksahzw', 'icxjvbrovtunleyzmdmfkhahgw', 'icxjvbroqtudleygpdmfksfhgw', 'bcxjvbroqtubllyzpdmfksahgw', 'icxwvbrontunzeyzpdmfksahgw', 'icxjvbroqtunleysjbmfksahgw', 'icxjvvroztunleyzpdmfksjhgw', 'ivxjxbroqtunleyzpdmfksahew', 'icxjvbroqtunleyupqufksahgw', 'icxjvmrcqtunleyzpdmxksahgw', 'icxjvgroqtunleyzpdgfkuahgw', 'icxjvbroqthnqeyfpdmfksahgw', 'icxjsbuodtunleyzpdmfksahgw', 'iuxjzbroqtunleyzpdrfksahgw', 'icxjvbrobtunlelzpdmfksahgs', 'icxjvbroqtzhljyzpdmfksahgw', 'inxtvbroqtunleyzpdmeksahgw', 'icgjvbroqtunleyztdmfksahgq', 'icxjvagoqtugleyzpdmfksahgw', 'icxuvbroqtunleyzpimfkyahgw', 'icxzvbroqtfhleyzpdmfksahgw', 'icxjjbroqtqnleyzpdmnksahgw', 'icjrvbroqtunleszpdmfksahgw', 'iexjvbroqtunlgyzpdmfksacgw', 'rcxjvbkoqtuoleyzpdmfksahgw', 'icxjvbroqgunlwyzpdmfksqhgw', 'icxjvbroqtunleqzpsmfksqhgw', 'icxjvbroqtubaeyzpdmfksaugw']
+
+# 1
+
+def has_repeats(id):
+	d = False
+	t = False
+	for l in id:
+		c = id.count(l)
+		if c == 2: d = True
+		if c == 3: t = True
+	return d, t
+
+doubles = []
+triples = []
+for id in ls:
+	d, t = has_repeats(id)
+	if d: doubles += [id]
+	if t: triples += [id]
+
+print(len(doubles) * len(triples))
+
+# 2
+
+def are_close(s1, s2):
+	dif = False
+	for i in range(len(s1)):
+		if s1[i] != s2[i]:
+			if dif: return False
+			else: dif = True
+	return True
+
+def find_boxes(ls):
+	for i in range(len(ls)):
+		id1 = ls[i]
+		for id2 in ls[i+1:]:
+			if are_close(id1, id2):
+				return id1, id2
+
+b1, b2 = find_boxes(ls)
+for i in range(len(b1)):
+	if b1[i] != b2[i]:
+		print(b1[:i] + b1[i+1:])
+		break

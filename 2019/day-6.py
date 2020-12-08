165f79ce3637a8a725d431719026f617709ffa6b
blx <benoit.linxe@gmail.com>
2020-12-09 00:56:06 +0100

    Use generators for 2019/6

--- 2019/day-6.py
+++ 2019/day-6.py
@@ -1,3 +1,17 @@
 with open('input-6.txt') as f:
 	input = f.read().split()
-input = { p[1]: p[0] for p in [i.split(')') for i in input] }
+
+orbits = { b[1]: b[0] for b in [i.split(')') for i in input] }
+
+def ancestors(b):
+	while b != 'COM':
+		b = orbits[b]
+		yield b
+
+# Part One
+print('checksum:', sum(sum(1 for _ in ancestors(b)) for b in orbits))
+
+# Part Two
+distYou, common = next((i, b) for i, b in enumerate(ancestors('YOU')) if b in ancestors('SAN'))
+distSan = next(i for i, b in enumerate(ancestors('SAN')) if b==common)
+print('jumps:', distYou+distSan)

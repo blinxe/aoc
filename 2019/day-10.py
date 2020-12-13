5c98a348922efeb4a7b8166a5968fa25bfbebaf5
blx <benoit.linxe@gmail.com>
2020-12-13 22:46:28 +0100

    Eeeewwwwwwww 2019/10

--- 2019/day-10.py
+++ 2019/day-10.py
@@ -7,6 +7,8 @@ import math
 W = len(input[0])
 H = len(input)
 
+# Part One
+
 def asteroids():
 	for x, y in itertools.product(range(W), range(H)):
 		if input[y][x] == '#':
@@ -41,4 +43,36 @@ def spot(base):
 	return n
 
 see = { a: spot(a) for a in asteroids() }
-print(see[max(see, key=lambda a: see[a])])
+base = max(see, key=lambda a: see[a])
+print(base, see[base])
+
+
+# Part Two
+mandist = [ (a, abs(a[0]-base[0]) + abs(a[1]-base[1])) for a in asteroids() ]
+mandist.sort(key = lambda a: a[1])
+
+def getangle(base, a):
+	x = a[0] - base[0]
+	y = a[1] - base[1]
+	angle = 180.0 + math.atan2(-x, y)*180/math.pi
+	if angle >= 360.0:
+		angle -= 360.0
+	return angle
+
+angles = [ (a[0], getangle(base, a[0]), a[1]) for a in mandist[1:] ]
+angles.sort(key = lambda a: a[1])
+
+nth = 0
+while angles:
+	remain = []
+	i = 0
+	while i<len(angles):
+		nth += 1
+		if nth == 200:
+			print(angles[i][0])
+		a = angles[i][1]
+		i += 1
+		while i<len(angles) and angles[i][1] == a:
+			remain.append(angles[i])
+			i += 1
+	angles = remain

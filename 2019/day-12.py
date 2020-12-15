ac8501482d56e6878e0cfe1d2b2e3bb8a29b3c27
blx <benoit.linxe@gmail.com>
2020-12-15 21:46:37 +0100

    Complete 2019/12. Yay.

--- 2019/day-12.py
+++ 2019/day-12.py
@@ -1,19 +1,10 @@
-pos = [
-	[6, -2, -7],
-	[-6, -7, -4],
-	[-9, 11, 0],
-	[-3, -4, 6],
-]
-
-vel = [
-	[0, 0, 0],
-	[0, 0, 0],
-	[0, 0, 0],
-	[0, 0, 0],
-]
-
-
-# Part One
+# sample
+# pos = [
+# 	[-1, 0, 2],
+# 	[2, -10, -7],
+# 	[4, -8, 8],
+# 	[3, 5, -1],
+# ]
 
 from itertools import combinations
 
@@ -23,7 +14,7 @@ def sign(x):
 	return 0
 
 def apply_gravity():
-	for m1, m2 in combinations(range(4), 2):
+	for m1, m2 in combinations(range(len(pos)), 2):
 		if m1 == m2: continue
 		p1 = pos[m1]
 		p2 = pos[m2]
@@ -32,18 +23,61 @@ def apply_gravity():
 		vel[m2] = [ v-d for v,d in zip(vel[m2], dv) ]
 
 def apply_velocity():
-	for m in range(4):
+	for m in range(len(pos)):
 		pos[m] = [ x+v for x,v in zip(pos[m], vel[m]) ]
+
+
+# Part One
+
+pos = [
+	[6, -2, -7],
+	[-6, -7, -4],
+	[-9, 11, 0],
+	[-3, -4, 6],
+]
+
+vel = [ [ 0 for _ in p ] for p in pos ]
 
 for _ in range(1000):
 	apply_gravity()
 	apply_velocity()
-	print(pos)
-	print(vel)
+
+pot = [ sum(abs(c) for c in p) for p in pos ]
+kin = [ sum(abs(c) for c in v) for v in vel ]
+
+print(sum(p*k for p,k in zip(pot, kin)))
 
 
-pot = [ abs(p[0]) + abs(p[1]) + abs(p[2]) for p in pos ]
-kin = [ abs(v[0]) + abs(v[1]) + abs(v[2]) for v in vel ]
+# Part Two
 
-total = sum(p*k for p,k in zip(pot, kin))
-print(total)
+pos = [
+	[6, -2, -7],
+	[-6, -7, -4],
+	[-9, 11, 0],
+	[-3, -4, 6],
+]
+
+from copy import deepcopy
+from math import gcd
+
+def lcm(a, b):
+	return a*b // gcd(a, b)
+
+initial = deepcopy(pos)
+
+steps = 1
+for dim in range(len(pos[0])):
+	n=0
+	pos = [ [p[dim]] for p in initial ]
+	vel = [ [ 0 for _ in p ] for p in pos ]
+	ref = (deepcopy(pos), deepcopy(vel))
+	print(pos, flush=True)
+	while True:
+		n += 1
+		apply_gravity()
+		apply_velocity()
+		if (pos, vel) == ref:
+			print(n, 'steps')
+			break
+	steps = lcm(steps, n)
+print(steps)
